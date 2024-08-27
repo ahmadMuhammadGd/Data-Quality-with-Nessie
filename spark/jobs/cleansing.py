6b25855db4145ba178e67f286b2c8cc219d6769a
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-08-27 03:30:58 +0300

    pass ingested object path in python bash command

---
+++ spark/jobs/cleansing.py
@@ -0,0 +1,138 @@
+
+
+
+
+# 1.  The code initializes a Spark session for data cleaning, 
+#     switches to a specific branch in the Nessie catalog, 
+#     and retrieves the most recent batch of data from a specified table.
+
+# 2.  It then applies a series of data cleaning strategies to the retrieved data, 
+#     such as dropping missing values, filtering negative values, validating date
+#     formats, and removing duplicates.
+
+# 3.  After cleaning, the code checks for spelling errors in certain columns 
+#     against a set of unique keys stored in CSV files.Finally, it prepares an
+#     SQL statement to insert the cleaned and validated data into another table
+#     within the Nessie catalog.
+
+
+
+
+
+import sys, os 
+sys.path.insert(1, '/spark')
+from modules.SparkIcebergNessieMinIO.spark_setup import init_spark_session
+from SparkCleaner import *
+import dotenv
+dotenv.load_dotenv('/config.env')
+
+NESSIE_CATALOG_NAME                 =os.getenv("NESSIE_CATALOG_NAME")       
+BRANCH_MAIN                         =os.getenv("BRANCH_MAIN")        
+BRANCH_VALIDATE                     =os.getenv("BRANCH_VALIDATE")        
+BRONZE_AMAZON_SALES_TABLE           =os.getenv("BRONZE_AMAZON_SALES_TABLE")        
+SILVER_AMAZON_SALES_TABLE           =os.getenv("SILVER_AMAZON_SALES_TABLE")        
+NAMESPACE                          =os.getenv("NAMESPACE")
+
+
+spark = init_spark_session(app_name="data cleaning")
+
+
+try:    
+    spark.sql(f"USE REFERENCE {BRANCH_VALIDATE} IN {NESSIE_CATALOG_NAME}")
+    df = spark.sql(
+        f"""
+        WITH recent_batch_date AS (
+            SELECT 
+                max(ingestion_date) AS max_date
+            FROM 
+                {NESSIE_CATALOG_NAME}.{NAMESPACE}.{BRONZE_AMAZON_SALES_TABLE}
+        ),
+        most_recent_batch AS (
+            SELECT 
+                * 
+            FROM 
+                {NESSIE_CATALOG_NAME}.{NAMESPACE}.{BRONZE_AMAZON_SALES_TABLE}
+            WHERE
+                ingestion_date = (
+                    SELECT
+                        max_date
+                    FROM
+                        recent_batch_date
+                )
+        )
+        SELECT * FROM most_recent_batch;
+        """
+    )
+
+    print('Most recent batch')
+    df.show()
+    
+    # now, this is the cleaning part !
+    cleaning_strategies=[
+        DropMissingValuesStrategy(columns=['Qty', 'Amount', 'Order_ID', 'order_Date', 'currency']),
+        FilterNegativeValuesStrategy(columns=['Qty', 'Amount']),
+        ValidateDatesStrategy(columns=['order_Date'], date_format="MM-dd-yy"),
+        DropDuplicatesStrategy(columns=['Order_ID', 'order_Date']),
+    ]
+    
+    cleaner = CleaningPipeline()
+    cleaner.set_dataframe(df=df)
+    cleaner.add_strategy(cleaning_strategies)
+    clean_df = cleaner.run()
+    
+    print('STAGE 1: cleaned patch')
+    print('source df:', df.count())
+    print('stage1: cleaned_df:', clean_df.count())
+    
+    
+    clean_df.createOrReplaceTempView("cleaned_batch")
+    # before loading, we need to check spelling for some columns
+    # picking up columns that has determined values
+    # such as: tags, size, status, category, etc
+    # this stage should discover if there any typos in these columns
+    
+    import glob
+    target_col_names = [os.path.splitext(os.path.basename(file_name))[0] for file_name in glob.glob('/unique_keys/*.csv')]
+    
+    # prepared sql
+    sql = f"""
+    CREATE OR REPLACE TABLE {NESSIE_CATALOG_NAME}.{NAMESPACE}.{SILVER_AMAZON_SALES_TABLE}_temp
+    USING ICEBERG AS
+    SELECT
+        {', '.join([
+            f'ROUND(src.{col}, 2) AS Amount' if col == 'Amount' else f'src.{col}'
+            for col in df.columns
+        ])}
+    FROM 
+        cleaned_batch AS src
+    WHERE
+        {" AND ".join([
+            f'''
+            src.{col} IN (
+                SELECT 
+                    {col}
+                FROM
+                    {NESSIE_CATALOG_NAME}.keys.{col}
+            )
+            '''.strip() for col in target_col_names if col != 'Date'
+        ])}
+    """
+    print('''
+\n\nSTAGE2 query:
+Create or replace a temporary table in Iceberg format with the cleaned data.
+- Select columns from `cleaned_batch`, rounding 'Amount' values to 2 decimal places.
+- Filter rows where column values match entries in corresponding reference tables.
+\n''', sql)
+    
+    spark.sql(sql)
+    
+    print('\n\nSTAGE 2: cleaned patch')
+    print('Source df:', df.count())
+    print('Stage1: cleaned_df:', clean_df.count())
+    print('Stage2: cleaned_df:', spark.sql(f"SELECT count(*) AS cnt FROM {NESSIE_CATALOG_NAME}.{NAMESPACE}.{SILVER_AMAZON_SALES_TABLE}_temp").collect()[0]['cnt'])
+    
+except Exception as e:
+    print (e)
+    raise
+finally:
+    spark.stop()
---
+++ spark/jobs/init_project.py
@@ -0,0 +1,133 @@
+import sys, os 
+sys.path.insert(1, '/spark')
+from modules.SparkIcebergNessieMinIO.spark_setup import init_spark_session
+import dotenv, glob
+dotenv.load_dotenv('/config.env')
+
+NESSIE_CATALOG_NAME                 =os.getenv("NESSIE_CATALOG_NAME")       
+BRANCH_MAIN                         =os.getenv("BRANCH_MAIN")        
+BRANCH_VALIDATE                     =os.getenv("BRANCH_VALIDATE")        
+BRONZE_AMAZON_SALES_TABLE           =os.getenv("BRONZE_AMAZON_SALES_TABLE")        
+SILVER_AMAZON_SALES_TABLE           =os.getenv("SILVER_AMAZON_SALES_TABLE")        
+NAMESPACE                          =os.getenv("NAMESPACE")
+
+MINIO_ACCESS_KEY                    = os.getenv('MINIO_ACCESS_KEY')
+MINIO_SECRET_KEY                    = os.getenv('MINIO_SECRET_KEY')
+MINIO_ICEBERG_S3_BUCKET             = os.getenv('MINIO_ICEBERG_S3_BUCKET')
+MINIO_LANDING_S3_BUCKET             = os.getenv('MINIO_LANDING_S3_BUCKET')
+MINIO_END_POINT                     = os.getenv('MINIO_END_POINT')
+
+spark = init_spark_session(app_name="tables and branches init")
+
+
+try:
+    spark.sql(f"CREATE NAMESPACE IF NOT EXISTS {NESSIE_CATALOG_NAME}.{NAMESPACE}")
+    spark.sql(f"CREATE NAMESPACE IF NOT EXISTS {NESSIE_CATALOG_NAME}.keys")
+    
+    print("Namespaces .. done")
+    spark.sql(f"DROP TABLE IF EXISTS {NESSIE_CATALOG_NAME}.{NAMESPACE}.{BRONZE_AMAZON_SALES_TABLE}")
+    spark.sql(f"DROP TABLE IF EXISTS {NESSIE_CATALOG_NAME}.{NAMESPACE}.{SILVER_AMAZON_SALES_TABLE}")
+    
+    spark.sql(
+        f"""
+        CREATE TABLE IF NOT EXISTS {NESSIE_CATALOG_NAME}.{NAMESPACE}.{BRONZE_AMAZON_SALES_TABLE} (
+            Order_ID            string,
+            order_Date          Date,
+            Status              string,
+            Fulfilment          string,
+            Sales_Channel       string,
+            ship_service_level  string,
+            Category            string,
+            Size                string,
+            Courier_Status      string,
+            Qty                 integer,
+            currency            string,
+            Amount              double,
+            ship_city           string,
+            ship_state          string,
+            ship_postal_code    integer,
+            ship_country        string,
+            B2B                 boolean,
+            fulfilled_by        string,
+            New                 string,
+            PendingS            string,
+            ingestion_date      TIMESTAMP
+        )
+        USING iceberg
+        PARTITIONED BY (MONTH(order_Date));
+        """
+    )
+    
+    spark.sql(
+        f"""
+        CREATE TABLE IF NOT EXISTS {NESSIE_CATALOG_NAME}.{NAMESPACE}.{SILVER_AMAZON_SALES_TABLE} (
+            Order_ID            string,
+            order_Date          Date,
+            Status              string,
+            Fulfilment          string,
+            Sales_Channel       string,
+            ship_service_level  string,
+            Category            string,
+            Size                string,
+            Courier_Status      string,
+            Qty                 integer,
+            currency            string,
+            Amount              double,
+            ship_city           string,
+            ship_state          string,
+            ship_postal_code    integer,
+            ship_country        string,
+            B2B                 boolean,
+            fulfilled_by        string,
+            New                 string,
+            PendingS            string,
+            ingestion_date      TIMESTAMP
+        )
+        USING iceberg
+        PARTITIONED BY (MONTH(order_Date));
+        """
+    )
+    
+    print("db tables .. done")
+
+    keys_files_list = glob.glob(f"/unique_keys/*.csv/")
+    print(f"keys_list: {keys_files_list}")
+    
+    for path in keys_files_list:
+        file_name = path.split("/")[-2]
+        file_name = file_name.split(".")[0]
+
+        df = spark.read \
+            .format("csv") \
+            .option("header", "true") \
+            .option("inferSchema", "true") \
+            .load(path)
+        
+        df.createOrReplaceTempView("temp")
+        spark.sql(f"""
+            CREATE OR REPLACE TABLE {NESSIE_CATALOG_NAME}.keys.{file_name}
+            USING iceberg
+            AS
+            SELECT
+                {file_name} AS {file_name}
+            FROM
+                temp
+        """)
+        spark.sql(f"""
+            SELECT 
+                * 
+            FROM {NESSIE_CATALOG_NAME}.keys.{file_name}
+            LIMIT 3
+        """).show()
+    
+    spark.sql(f"CREATE BRANCH IF NOT EXISTS {BRANCH_VALIDATE} IN {NESSIE_CATALOG_NAME}")
+    
+    spark.sql('show tables in nessie').show()
+    spark.sql('LIST REFERENCES in nessie').show()
+    
+    spark.stop()
+except Exception as e:
+    print (e)
+    raise
+finally:
+    spark.stop()
