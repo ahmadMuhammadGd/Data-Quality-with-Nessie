7d375d81b3bdb97d8fa78df0b4c0aa53c3561960
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-08-27 03:33:22 +0300

    each file has a simple functionality

--- run.sh
+++ run.sh
@@ -1,38 +1,28 @@
 # run it manually without airflow
 set -e 
 
-docker compose up -d
+# docker compose up -d
 
-# write on env file to upate minio end point
-# Making sure that the minio ip has been updated in spark container
-minio_container_name='minio'
-container_id=$(docker container ls -q --filter "name=${minio_container_name}")
-if [ -z "$container_id" ]; then
-  echo "container ${minio_container_name} is not found"
-  exit 1
-fi
+# docker compose restart spark
 
-container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
-echo "Minio IP: ${container_ip}"
-sed -i "s|^MINIO_END_POINT.*|MINIO_END_POINT=http://${container_ip}:9000|" config.env
-
-docker compose restart spark
-
-# in this pipeline version, it ALWAYS uses the corupted dataframe 
-docker exec spark spark-submit /spark-jobs/jobs/init_project.py \
+# # creates landing file bucket
+# chmod +x minio-setup.sh
+# sh minio-setup.sh
+# initializes Iceberg tables
+docker exec spark spark-submit /spark/jobs/init_project.py\
     > bash-logs/0-init_job.log
 
-docker exec spark spark-submit /spark-jobs/jobs/ingest.py \
+docker exec spark spark-submit /spark/jobs/ingest.py -o queued/sampled_data_1.csv\
     > bash-logs/1-ingest.log
 
-docker exec spark spark-submit /spark-jobs/soda-jobs/checks/bronz_amazon_sales.py \
+docker exec spark spark-submit /soda/checks/bronz_amazon_sales.py \
     > bash-logs/2-bronze_validation.log
 
-docker exec spark spark-submit /spark-jobs/jobs/cleansing.py \
+docker exec spark spark-submit /spark/jobs/cleansing.py \
     > bash-logs/3-cleansing.log
 
-docker exec spark spark-submit /spark-jobs/soda-jobs/checks/silver_amazon_sales.py \
+docker exec spark spark-submit /soda/checks/silver_amazon_sales.py \
     > bash-logs/5-silver_validation.log
 
-docker exec spark spark-submit /spark-jobs/jobs/load.py \
+docker exec spark spark-submit /spark/jobs/load.py \
     > bash-logs/6-load.log
