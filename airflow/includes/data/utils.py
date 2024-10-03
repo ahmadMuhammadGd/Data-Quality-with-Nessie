2fc511c94235a7fe990a167725b02296a130644b
Ahmad Muhammad <121247258+ahmadMuhammadGd@users.noreply.github.com>
2024-10-03 21:44:15 +0000

    feature: update outlet dataset and get extra from triggering dataset

--- airflow/includes/data/utils.py
+++ airflow/includes/data/utils.py
@@ -1,25 +1,8 @@
 from datetime import datetime
-def custom_extra_template(file_name:str, branch_name: str) -> dict:
-    """
-    Generates a dictionary containing metadata about a file and branch, including the current timestamp.
-
-    Args:
-        file_name (str): The name of the file to be included in the metadata.
-        branch_name (str): The name of the branch associated with the file.
-
-    Returns:
-        dict: A dictionary with the following keys:
-            - "file_name": The provided file name.
-            - "branch_name": The provided branch name.
-            - "updated_at": The current timestamp in the format '%Y-%m-%dT%H:%M:%S'.
-    """
-    return {
-        "file_name":    file_name,
-        "branch_name":  branch_name,
-        "updated_at":   datetime.now().strftime("%Y-%m-%dT%H:%M:%S") 
-    }   
+from airflow.datasets import Dataset
+import json  
     
-def get_extra_triggering_run(context) -> list:
+def get_extra_triggering_run(context)->list:
     """
     Extracts the 'extra' metadata from the first dataset in each triggering event 
     and returns the 'extra' metadata list of all processed datasets.
@@ -32,5 +15,16 @@ def get_extra_triggering_run(context) -> list:
     result = []
     for _, dataset_list in triggering_dataset_events.items():
         if dataset_list:
-            extra = dataset_list[0].extra  
-            return extra
+            result.append(dataset_list[0].extra)
+    return result
+
+
+from typing import Union
+def update_outlet ( dataset: Dataset, 
+                    content: Union[str, dict, int, float, list, bool] = None,
+                    context: dict = None
+    ):
+    if context is None:
+        context = {}
+    context["outlet_events"][dataset].extra = content 
+    
